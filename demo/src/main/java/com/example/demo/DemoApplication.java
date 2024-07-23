c307912f1500570d9238d625cbf642f376a99da3
khizar <mohammedkhizar501@gmail.com>
2024-07-23 18:59:05 +0530

    test

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+	public static void main(String[] args) {
+		log.info("Khizar Ayub");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Khizar Ayub in CommandLineRunner Override method");
+		
+	}
+
+}
